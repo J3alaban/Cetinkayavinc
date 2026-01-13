d6a3fa296026fde970f56c5b7c15ccc85ee22885
J3alaban <149269049+J3alaban@users.noreply.github.com>
2026-01-13 18:15:07 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     legacy({
-      targets: ['defaults', 'not IE 11'],
+      targets: ['>0.2%', 'not dead', 'not op_mini all', 'ie 11'], 
+      renderLegacyChunks: true,
+      polyfills: true
     }),
   ],
 })
